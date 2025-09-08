badbeac4fb35a6c36706d69a339a8b5e1d030440
Matrix <matrixzhang98@gmail.com>
2025-09-08 15:55:34 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SeleniumAutomation',
+    version='1.0.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[],
+    author='Matrix Zhang',
+    description='Selenium automation testing project',
+)
